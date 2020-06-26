c4703e36ef3a4f844c1f3219df45410f7ed830e5
James Spicer <spicer.r.james@gmail.com>
2020-06-26 13:55:18 -0500

    working on functionalityt

--- calc.js
+++ calc.js
@@ -23,3 +23,27 @@ function getFormattedNumber(num) {
     var value = n.toLocaleString("en");
     return value;
 }
+
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+
+var operator = document.getElementsByClassName("operator");
+
+for (var i = 0; i < operator.length; i++){
+    operator[i].addEventListener('click', function(){
+
+    })
+}
+
+var number = document.getElementsByClassName("number");
+
+for (var i = 0; i < number.length; i++){
+    number[i].addEventListener('click', function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output != NaN){
+            output=output + this.id;
+            printOutput(output);
+        }
+    });
+}
